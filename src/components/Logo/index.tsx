ec7db6215a70de582cfb606a6e8cfb69e06956e4
Denis Forigo <denisforigo@gmail.com>
2024-06-19 20:22:00 -0300

    add unoptimized={true}

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 
 export default function Logo() {
 	return (
-		<Image src={'/EstelaLuz.png'} alt={"Logo Extela Luz"} width={171} height={22} />)
+		<Image src={'/EstelaLuz.png'} unoptimized={true} alt={"Logo Extela Luz"} width={171} height={22} />)
 }
