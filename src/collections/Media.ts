0192421a0fbf0c2d623d8706f8ff40b61112b41b
Denis Forigo <denisforigo@gmail.com>
2024-06-19 11:45:09 -0300

    change the media folder

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -14,7 +14,7 @@ export const Media: CollectionConfig = {
     },
   ],
   upload: {
-    staticDir: "media",
+    staticDir: "public",
     imageSizes: [
       {
         name: "thumbnail",
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -32,7 +32,7 @@ async function getData(cat: number) {
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
-  cors: [process.env.PAYLOAD_PUBLIC_SERVER_URL || ""].filter(Boolean),
+  cors: "*",
   csrf: [process.env.PAYLOAD_PUBLIC_SERVER_URL || ""].filter(Boolean),
   admin: {
     user: Users.slug,
