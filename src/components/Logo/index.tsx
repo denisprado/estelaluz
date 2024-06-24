1a1161ee1d3c52c999c04142019b55fe509e6808
Denis Forigo <denisforigo@gmail.com>
2024-06-24 15:02:09 -0300

    fix use client um loaders

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,8 +1,9 @@
+import imageLoader from "@/helpers/loader";
 import Image from "next/image";
 
 export default function Logo() {
 	return (
-		// <Image src={'/EstelaLuz.png'} unoptimized={true} alt={"Logo Extela Luz"} width={171} height={22} />
-		<div className={"bg-[url('/EstelaLuz.png')] w-[171px] h-[22px] bg-no-repeat"} />
+		<Image loader={imageLoader} src={'/EstelaLuz.png'} unoptimized={true} alt={"Logo Extela Luz"} width={171} height={22} />
+
 	)
 }
