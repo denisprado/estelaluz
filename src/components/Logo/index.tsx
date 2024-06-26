66948398f251cb1671e1c6638fe82ab0fa6cdb72
Denis Forigo <denisforigo@gmail.com>
2024-06-26 01:30:26 -0300

    fix logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Logo() {
 	return (
-		<Image loader={imageLoader} src={'/EstelaLuz.png'} unoptimized={true} alt={"Logo Extela Luz"} width={171} height={22} />
+		<Image loader={imageLoader} src={'/EstelaLUZ.png'} alt={"Logo Estela Luz"} width={171} height={22} />
 
 	)
 }
