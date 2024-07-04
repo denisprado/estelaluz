817f840140a6bdbfd777243a240d93eb6bef2fb1
Denis Forigo <denisforigo@gmail.com>
2024-07-03 21:45:23 -0300

    change hover

--- src/components/TextLink/index.tsx
+++ src/components/TextLink/index.tsx
@@ -2,6 +2,6 @@ import Link from "next/link";
 import classNames from "classnames";
 export default function TextLink({ url, text, className }: { url: string, text: string, className?: string }) {
 	return (
-		<Link href={url} className={classNames("hover:font-bold block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-700 md:p-0 md:dark:hover:text-gray-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white transition-all delay-300 md:dark:hover:bg-transparent dark:border-gray-700", className)}>{text}</Link>
+		<Link href={url} className={classNames("hover:underline hover:underline-offset-2 block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-700 md:p-0 md:dark:hover:text-gray-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white transition-all delay-300 md:dark:hover:bg-transparent dark:border-gray-700", className)}>{text}</Link>
 	)
 }
--- src/components/WhatsAppBtn/index.tsx
+++ src/components/WhatsAppBtn/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { FloatingWhatsApp } from "react-floating-whatsapp";
 
-const FloatBtn = () => <FloatingWhatsApp phoneNumber={"5519983069346"} accountName={"Estela Luz"} chatMessage="Olá! Como posso te ajudar?" statusMessage="online" />
+const FloatBtn = () => <FloatingWhatsApp phoneNumber={"5519983069346"} accountName={"Estela Luz"} chatMessage="Olá! Como posso te ajudar?" statusMessage="online" allowClickAway />
 
 export default FloatBtn
