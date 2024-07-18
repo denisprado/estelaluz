c780be1a6ae0298fec4caa82c8300799ed1bf9a5
Denis Forigo <denisforigo@gmail.com>
2024-07-18 14:12:36 -0300

    map working

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,5 @@
 
 import { Work } from "@/payload-types";
-import DynamicMap from "./dinamycMap";
 
 const Map = ({ work, height = '800' }:
 	{
@@ -9,18 +8,37 @@ const Map = ({ work, height = '800' }:
 		height?: string;
 	}) => {
 
+	const mapParameters = work.mapUrl ? extractMapParametersFromUrl(work.mapUrl) : null;
+
+	function extractMapParametersFromUrl(url: string) {
+		const regex = /@(-?\d+\.\d+),(-?\d+\.\d+),(\d+)a,(\d+\.\d+)y,(\d+\.\d+)h,(\d+\.\d+)t/;
+		console.log("url", url)
+
+		const matches = url.match(regex);
+		console.log("matches", matches)
+		if (matches) {
+			return {
+				lat: parseFloat(matches[1]),
+				lng: parseFloat(matches[2]),
+				zoom: parseInt(matches[3]),
+				pitch: parseFloat(matches[4]) * .1,
+				heading: parseFloat(matches[5]),
+				tilt: parseFloat(matches[6]),
+			};
+		} else {
+			return null;
+		}
+	}
 
 	return (
-		work.mapUrl &&
-		<DynamicMap mapUrl={work.mapUrl} />
-
+		// <DynamicMap mapUrl={work.mapUrl} />
+		work.mapUrl && mapParameters &&
+		<div className="w-full">
+			<iframe src={`https://www.google.com/maps/embed/v1/streetview?key=AIzaSyBKTYFjiaeFIsPiDrTTTUKxYxDI6-h-_S0&location=${mapParameters!.lat},${mapParameters!.lng!}&heading=${mapParameters!.heading}&pitch=${mapParameters!.pitch}&fov=50&`} width="100%" height={height} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+		</div>
 	)
 };
 
-// props.hasCoordinates(props.work?.coordenadas) && <div className="w-full">
-// 	<iframe src={`https://www.google.com/maps/embed/v1/streetview?key=AIzaSyBFQ2XvHhidZ5rCrKnJ9GZMzldlkTUsRKQ&location=${props.work?.coordenadas?.longitude!},${props.work?.coordenadas?.latitude!}&heading=161&pitch=10&fov=35&`} width="100%" height={height} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-// </div>
-
-
-
 export default Map
+
+
