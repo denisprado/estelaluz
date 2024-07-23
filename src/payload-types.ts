30918b084d23f4947d078909176621a72c26c514
Denis Forigo <denisforigo@gmail.com>
2024-07-23 02:44:35 -0300

    codemode

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -22,6 +22,9 @@ export interface Config {
     'payload-preferences': PayloadPreference;
     'payload-migrations': PayloadMigration;
   };
+  db: {
+    defaultIDType: number;
+  };
   globals: {};
   locale: null;
   user: User & {
@@ -33,12 +36,15 @@ export interface UserAuthOperations {
     email: string;
   };
   login: {
-    password: string;
     email: string;
+    password: string;
   };
   registerFirstUser: {
     email: string;
     password: string;
+  };
+  unlock: {
+    email: string;
   };
 }
 /**
